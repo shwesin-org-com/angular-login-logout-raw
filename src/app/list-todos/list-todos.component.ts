6e1dfcd516a9e52d6b0308d19581596a3a27b31a
ShweSinHtay <shwesin@isgm2.com>
2020-09-08 17:35:59 +0630

    Todo class create in todo typescript

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {}
+}
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,10 +14,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
   todos = [
-    {id: 1, description: 'Learn to Dance' },
-    {id: 2, description: 'Learn to Angular'},
-    {id: 3, description: 'Learn to Spring Boot'},
-    {id: 4, description: 'Learn to React'}
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Learn to Angular', false, new Date()),
+    new Todo(3, 'Learn to React', false, new Date()),
+    new Todo(4, 'Learn to Spring Boot', false, new Date())
+    // { id: 1, description: 'Learn to Dance' },
+    // { id: 2, description: 'Learn to Angular' },
+    // { id: 3, description: 'Learn to Spring Boot' },
+    // { id: 4, description: 'Learn to React' }
   ];
   // todo = {
   //   id: 1,
