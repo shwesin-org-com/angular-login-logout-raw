186b69fead3ea5b427f80873470fb146ade95b3e
ShweSinHtay <shwesin@isgm2.com>
2020-09-08 14:30:19 +0630

    routing added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  username = 'shwesin';
+  password = '';
+  errorMessage = 'Invalid Creditial';
+  invalidLogin = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin() {
+    // console.log(this.username);
+    if (this.username === 'shwesin' && this.password === 'shwesin') {
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
   }
 
 }
