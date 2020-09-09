c76b9519d711330aaa94378e5d75575af05eb7e8
ShweSinHtay <shwesin@isgm2.com>
2020-09-09 13:50:18 +0630

    login, logout session storage

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  // isUserLoggedIn = false;
+  constructor(public hardcodedAuthenticationService:
+    HardcodedAuthenticationService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    // this.isUserLoggedIn = this.hardcodedAuthenticationService.isUserLoggedIn();
   }
 
 }
--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -7,10 +7,21 @@ export class HardcodedAuthenticationService {
 
   constructor() { }
   authenticate(username, password) {
+    console.log('before' + this.isUserLoggedIn());
     if (username === 'shwesin' && password === 'shwesin') {
+      sessionStorage.setItem('authenticaterUser', username);
+      console.log('after' + this.isUserLoggedIn());
       return true;
     } else {
       return false;
     }
+  }
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticaterUser');
+    return !(user === null);
+  }
+
+  logout() {
+    sessionStorage.removeItem ('authenticaterUser');
   }
 }
